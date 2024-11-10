42729cc8c88f8704ebf04a1a962b0f4a0881d814
sohelx196 <sohel.devloper196@gmail.com>
2024-11-10 15:18:23 +0530

    Changed In Main Branch

---
+++ API's/api.js
@@ -0,0 +1,4 @@
+//  Api --> Application Programming Interface
+// Api based on request and response cycle cause we request a data from server and api give us response or give us data
+
+// We use fetch() method to fetch data 
