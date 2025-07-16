acf5ad065709da652c1663bf3d31a8c4fde689dc
sohelx196 <sohel.devloper196@gmail.com>
2025-07-16 08:48:41 +0530

    iifee

--- Basic JS/Functions & More/IIFE_7.js
+++ Basic JS/Functions & More/IIFE_7.js
@@ -1,3 +1,6 @@
 // IIFE ( Immediately Invoked Function Expression ) -->
 // It's a function expression wrapped in () and then invoked immediately with ()
 
+(function iifeChecking(){
+    console.log("This is a iife function...")
+})();
