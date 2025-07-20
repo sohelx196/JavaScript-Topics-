8a47fe1d2e47cd6596692b0910fa39d39ef9b754
sohelx196 <sohel.devloper196@gmail.com>
2025-07-20 12:03:08 +0530

    WORKING ON DOM

--- Dom/dom.js
+++ Dom/dom.js
@@ -37,8 +37,6 @@
 
 
 
-
-
 // To access element in js there are some ways---
 // 1.) By Id --
  
@@ -60,7 +58,7 @@ console.dir(obj3);
 
     //   Better Way to acces element is ==>
     
-    //   4.) Query Selector -- In it we can pass the above all selector which is id,class,tagName
+    //   4.) Query Selector -- In it we can pass the above  all selector which is id,class,tagName
     //   One element of html is called Node and 2 or more nodes are known as nodeList
 
     // querySelector -- by tagName
