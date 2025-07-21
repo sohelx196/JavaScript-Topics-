1ec9c3cad77912648a26c53ea65cf8d4c554cf24
sohelx196 <sohel.devloper196@gmail.com>
2025-07-21 21:59:50 +0530

    D-DOM MANIPULATION

--- Dom/DynamicDomManipulation_02.js
+++ Dom/DynamicDomManipulation_02.js
@@ -4,7 +4,14 @@
     // create element first
     // then append/prepend karo jaha bhi element chahiye
 
+    // 🔹 append()
+       // Adds content to the end of a selected element.
+
+    // 🔸 prepend()
+      // Adds content to the beginning of a selected element.
+
 
 let firstElement = document.createElement("h1");
 firstElement.textContent = "Hello , this is created by JS";
 document.querySelector("body").prepend(firstElement);
+
