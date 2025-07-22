d1eca8de64663146309f83bdc1459e74e827fc5a
sohelx196 <sohel.devloper196@gmail.com>
2025-07-22 12:30:08 +0530

    dom manipulation

--- Dom/DynamicDomManipulation_02.js
+++ Dom/DynamicDomManipulation_02.js
@@ -15,3 +15,24 @@ let firstElement = document.createElement("h1");
 firstElement.textContent = "Hello , this is created by JS";
 document.querySelector("body").prepend(firstElement);
 
+
+
+
+// changing css using Js ==>
+   // using .style
+let cssChange = document.querySelector("#messi")
+cssChange.style.color = "pink";
+
+   // using .classList (add , remove , toggle)
+let h3 = document.querySelector("h3");
+h3.classList.add("myClass")
+
+// h3.classList.remove("myClass")   // it will remove the class
+
+// h3.classList.toggle("myClass")   // it is used to switch the current class
+
+
+
+
+
+
