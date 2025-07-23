3c14b23bfc7e5cb441ac5894b18709463e80bade
sohelx196 <sohel.devloper196@gmail.com>
2025-07-23 17:58:22 +0530

    working

--- Mini Projects/Mini Features/Upload.js
+++ Mini Projects/Mini Features/Upload.js
@@ -6,6 +6,30 @@ upload.addEventListener("click" , ()=>{
 })
 
 file.addEventListener("change" , (val)=>{
-   
+   upload.textContent = val.target.files[0].name;
 })
+
+
+
+// Limit feature js -->
+let textarea = document.querySelector("#textarea");
+let limit = document.querySelector("#limit");
+
+let maxLimit = 20;
+
+textarea.addEventListener("input" , ()=>{
+     let lenght =  textarea.value.length;
+     limit.textContent = `Limit : ${lenght}/${maxLimit}`
+
+     if(lenght>maxLimit){
+        limit.style.color = "red"
+        limit.textContent = "Limit is crossed"
+     }else{
+          limit.style.color = "white"
+     }
+})
+
+
+
+// Password field js -->
 
