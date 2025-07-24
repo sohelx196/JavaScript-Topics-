e1d4b7d5f84ba8c66871f657224fed6afa73f052
sohelx196 <sohel.devloper196@gmail.com>
2025-07-24 21:16:35 +0530

    learning form handeling

--- Mini Projects/Mini Features/FormSubmit.js
+++ Mini Projects/Mini Features/FormSubmit.js
@@ -1,7 +1,8 @@
 // we need to stop the default submit of form cause it is reloading the page
 
 let form = document.querySelector("form");
-let inputs = document.querySelectorAll("input")
+let inputs = document.querySelectorAll("input");
+let 
 
 form.addEventListener("submit" , (detail)=>{
     detail.preventDefault();    // now the form is not submitted default
