a0a7b3284d2a25424c5a66a200356082f9bde3c1
sohelx196 <sohel.devloper196@gmail.com>
2025-07-25 14:35:46 +0530

    working on Form handling

--- Mini Projects/Mini Features/FormSubmit.js
+++ Mini Projects/Mini Features/FormSubmit.js
@@ -2,11 +2,41 @@
 
 let form = document.querySelector("form");
 let inputs = document.querySelectorAll("input");
-let 
+let myForm = document.querySelector(".myForm");
 
 form.addEventListener("submit" , (detail)=>{
     detail.preventDefault();    // now the form is not submitted default
+
+    let card = document.createElement("div")
+    card.classList.add("card");
+
+    let profile = document.querySelector("div");
+    profile.classList.add("profile");
+
+
+    let image = document.createElement("img");
+    image.setAttribute("src" , "https://plus.unsplash.com/premium_photo-1689568126014-06fea9d5d341?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D");
+
+    let h1 = document.createElement("h1");
+    h1.textContent = "Sohel Sheikh";
+
+    let job = document.createElement("p");
+    job.textContent = "Coder";
+
+    let email = document.createElement("p");
+    email.textContent = "sohel@gmail.com";
+
+    
+    profile.appendChild(image);
+    card.appendChild(profile);
+
+    card.appendChild(h1);
+    card.appendChild(job);
+    card.appendChild(email);
+
+    myForm.appendChild(card)
     
 });
 
 
+   
