5f6b7026ef726be93eae7e60af66577ce0e30246
sohelx196 <sohel.devloper196@gmail.com>
2025-07-26 18:30:27 +0530

    working

---
+++ Time&Interval/MiniProject/typingTest.js
@@ -0,0 +1,11 @@
+
+
+let sentence = document.querySelector("#sentence");
+let typingBox = document.querySelector("#typingBox");
+let startBtn = document.querySelector("#startBtn");
+let time = document.querySelector("#timer");
+let result = document.querySelector("#result");
+
+startBtn.addEventListener("click" , ()=>{
+    typingBox
+})
