e61746b802136a6248b4e6cb0d17140b782804fa
sohelx196 <sohel.devloper196@gmail.com>
2025-07-29 20:21:37 +0530

    making project

--- FinalProject/Contact.js
+++ FinalProject/Contact.js
@@ -1,19 +1,36 @@
-let newInput = document.querySelector("#newInput");
-let defaultFile = document.querySelector("#defaultFile");
 
 
-// Custom Upload button
-newInput.addEventListener("click" , ()=>{
-   defaultFile.click();
+let form = document.querySelector("form");
+let inputs = document.querySelectorAll("input")
+
+form.addEventListener("submit" , (val)=>{
+   val.preventDefault();
+
+   let main = document.createElement("div");
+   main.classList.add("contacts");
+
+   let profile = document.querySelector("div");
+   profile.classList.add("profile");
+
+   let image = document.createElement("image");
+   image.setAttribute("src" , "https://images.unsplash.com/photo-1494790108377-be9c29b29330?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D");
+
+   let footBaller = document.createElement("h3");
+   footBaller.textContent = "Sohel Sheikh";
+
+   let contact = document.createElement("p");
+   contact.textContent = "934739472334";
+
+
+   profile.append(image);
+   main.append(profile);
+   main.append(footBaller)
+   main.append(contact)
+
+   
+
+
+
 })
 
-defaultFile.addEventListener("change",(val)=>{
-   newInput.textContent =  val.target.files[0].name
-})
 
-
-
-let inputs = document.querySelector("input");
-inputs.addEventListener("input" , (val)=>{
-   console.log(val)
-});
