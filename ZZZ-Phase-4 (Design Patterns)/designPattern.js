3cb2a0cc37799f9fa2ae45de7f7ada5ec7346cb9
sohelx196 <sohel.devloper196@gmail.com>
2025-10-12 09:40:55 +0530

    module pattern

--- ZZZ-Phase-4 (Design Patterns)/designPattern.js
+++ ZZZ-Phase-4 (Design Patterns)/designPattern.js
@@ -1,11 +1,15 @@
-// Design Pattern -> 
+// Design Pattern -> A smart way to organize or structure our code to make it more readable, reusable, and maintainable. 
   
-   // Module pattern => 
+   // *** Module pattern *** => 
+      //  The Module Pattern is one of the most popular design patterns in JavaScript.
+      //    It is used to:
 
+      //    Encapsulate (hide) private data and functions
+         
+      //    Expose only what you want (public methods)
+         
+      //    Avoid polluting the global scope
+               //  Global variable pollution = having too many variables in the global scope that can accidentally overwrite each other.
 
-// used for making private var and fncs
+               //  Module Pattern helps by creating private scopes, protecting your variables from being exposed globally.
 
-
-let Bank = () => {
-   let balance = 1000;
-};
