a9acb899409dc7984b393a9688605bc6dd1f61b6
sohelx196 <sohel.devloper196@gmail.com>
2025-10-15 14:22:15 +0530

    working

--- ZZZ-Phase-4 (Design Patterns)/designPattern.js
+++ ZZZ-Phase-4 (Design Patterns)/designPattern.js
@@ -58,10 +58,11 @@ let Bank = (function(){
   function statement(){
     console.log("Transactions" , transaction)
   }
-
-  return{ withdraw , addBalance , statement }
+  
+  return{ checkbalance , withdraw , addBalance , statement }
 })(); 
 // Bank.withdraw(100);
-// Bank.addBalance(1100);
+// Bank.addBalance(1100); 
 // Bank.statement()
 // Bank.addBalance(2000);
+
